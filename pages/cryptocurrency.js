32dbeb130f8677ccfd2c51adfec53f509cd7ab7a
DanielRSnell <hi@danielsnell.ninja>
2018-08-09 13:27:51 -0400

    public

--- pages/cryptocurrency.js
+++ pages/cryptocurrency.js
@@ -8,6 +8,7 @@ import Layout from '../components/layout'
 import {Router} from '../server/router'
 import { Row, Col } from 'antd'
 
+
 //Request
 import { request } from 'graphql-request'
 
@@ -18,7 +19,7 @@ import PageHeader from '../components/coinpage/header'
 //endpoints
 const endpoint = 'https://hackcoin-ssr.herokuapp.com/graphql'
 
-const CoinQuery = gql`
+const CoinQuery = `
 query CoinPage($symbol: String!, $currency: String!, $limit: Int!) {
   getInfo(symbol: $symbol) {
     rank
@@ -75,29 +76,53 @@ query CoinPage($symbol: String!, $currency: String!, $limit: Int!) {
   }
 }`;
 
-const CoinPage = ({CoinPage}) => {
+const CoinPage = ({Info, Hist, Stats}) => {
   
-  const id = 'test'
-  const name = 'test'
-  const symbol = 'test'
-
+  console.log(Info);
+  console.log(Hist);
+  console.log(Stats);
+  const { id } = Info[0];
+  const { name, symbol } = Stats;
+  console.log(id);
   return (
     <Layout favicon={id} title={`${name} ${symbol} price, charts and detailed metrics`} description={`${name} ${symbol} historic and live price charts from all exchanges. Find all related cryptocurrency info and read about ${name}'s latest news`}>
-      This is a test
+      <PageHeader data={Info} source={Stats} />
+      <CryptoChart data={Hist} name={name}/>
     </Layout>
   )
 }
 
-CoinPage.getInitialProps = async ({req}, apolloClient, context) => {
-  
-  const { id } = await req.params;
-  const keys = {
+CoinPage.getInitialProps = async (context) => {
+
+
+  const { id } = context.req.params;
+  if ( id !== 'MIOTA') {
+  const variables = await {
     symbol: id,
     currency: "USD",
     limit: 365
-  }
-  return await apolloClient.query({query: CoinQuery}, {variables: keys});
+   }
+
+   const find = await request(endpoint, CoinQuery, variables)
+   .then(data => {
+     const { fetchCoin, GetHistory, getInfo } = data;
+     return { Info: fetchCoin, Hist: GetHistory, Stats: getInfo }
+   });
+  return find; 
+  } else if (id === 'MIOTA') {
+    const variables = await {
+      symbol: "IOTA",
+      currency: "USD",
+      limit: 365
+     }
   
+     const find = await request(endpoint, CoinQuery, variables)
+     .then(data => {
+       const { fetchCoin, GetHistory, getInfo } = data;
+       return { Info: fetchCoin, Hist: GetHistory, Stats: getInfo }
+     });
+    return find;
+  }
 }
 
 
